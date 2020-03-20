bcd3e9005ec05ce47c3002ed33ff4769b46c0e82
Sasha <cherevko.alexandr.1997@gmail.com>
2020-03-20 20:03:15 +0200

    first commit

---
+++ src/hw15/Main.java
@@ -0,0 +1,22 @@
+package hw15;
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.stream.IntStream;
+
+public class Main {
+    private static final int Size = 10;
+    private static final int min = 10;
+    private static final int max = 20;
+    private static final int diff = (max - min) + 1;
+
+    public static void main(String[] args) {
+        int[] array = new int[Size];
+        Random random = new Random();
+        for (int i = 0; i < Size; i++) {
+          array[i] = random.nextInt(diff + min);
+        }
+        IntStream streamFromArrays = Arrays.stream(array);
+        System.out.println(streamFromArrays.map((s) -> s * s).average());
+    }
+}
